156f38a2cbf687117a3189f3bc8cee2f44f93a43
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-10-12 13:21:29 +0200

    Store & Reduce works but not the printed value

--- index.ios.js
+++ index.ios.js
@@ -9,23 +9,58 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
+import { createStore } from 'redux';
+
+
+
+
+
+
 
 class Fil_Rouge extends Component {
+    constructor(props) {
+      super(props);
+      this.store = createStore(this.counter);
+    }
+
+    counter(state = 0, action) {
+      switch(action.type) {
+        case 'INCREMENT':
+          return state + 1;
+        break;
+        case 'DECREMENT':
+          return state - 1;
+        break;
+        default:
+          return state;
+        break
+      }
+    }
+
+    decrement() {
+        this.store.dispatch({type: 'DECREMENT'});
+    }
+
+    increment() {
+        this.store.subscribe( () => {
+            console.log("Store change", this.store.getState().toString());
+        });
+        this.store.dispatch({type: 'INCREMENT'});
+    }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TouchableHighlight onPress={() => this.decrement()}>
+            <Text>-1</Text>
+        </TouchableHighlight>
+        <Text>{this.store.getState()}</Text>
+        <TouchableHighlight onPress={() => this.increment()}>
+            <Text>+1</Text>
+        </TouchableHighlight>
       </View>
     );
   }
