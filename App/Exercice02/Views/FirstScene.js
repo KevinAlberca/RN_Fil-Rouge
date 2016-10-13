31563b3972bab006adbc630f7e392dab301202d4
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-10-13 17:31:50 +0200

    Change view

---
+++ App/Exercice02/Views/FirstScene.js
@@ -0,0 +1,46 @@
+/**
+ * Created by AwH on 10/13/16.
+ */
+import React, { Component, PropTypes } from 'react';
+import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+
+export default class FirstScene extends Component {
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        onForward: PropTypes.func.isRequired,
+        onBack: PropTypes.func.isRequired,
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Current Scene: { this.props.title }</Text>
+                <TouchableHighlight onPress={this.props.onForward}>
+                    <Text>Tap me to load the next scene</Text>
+                </TouchableHighlight>
+                <TouchableHighlight onPress={this.props.onBack}>
+                    <Text>Tap me to go back</Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
