7ff00a0dc9039c0e0cc6e1874824f1275887748a
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-10-26 15:05:25 +0200

    Add Exercice02 correction

---
+++ App/Exercice02/Correction.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Navigator,
+    TouchableOpacity
+} from 'react-native';
+
+const routes = [
+    {title: 'First Scene', index: 0},
+    {title: 'Second Scene', index: 1},
+    {title: 'Third Scene', index: 2},
+    {title: 'Fourth Scene', index: 3},
+];
+
+export default class Exercice02C extends Component {
+    render() {
+        return (
+            <Navigator
+                initialRoute={{ title: 'Awesome Scene', index: 0 }}
+                renderScene={(route, navigator) => {
+                    return <View style={styles.container}>
+                        <TouchableOpacity onPress={() => {
+                            if (routes[route.index+1]) {
+                                navigator.push(routes[route.index+1])
+                            } else {
+                                navigator.popToTop();
+                            }
+                        }}>
+                            <Text style={styles.text}>Hello {route.title}!</Text>
+                            <Text>Scene {route.index}</Text>
+                        </TouchableOpacity>
+                    </View>
+                }}
+                style={{paddingTop: 30}}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#F5FCFF',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 15
+    }
+});
+
+AppRegistry.registerComponent('Exercice02C', () => Exercice02C);
