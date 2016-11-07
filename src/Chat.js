37de025bf0958c96ec7e385a2d49accf0f1c0399
Kevin Alberca <kevin.alberca@supinternet.fr>
2016-11-07 19:17:20 +0100

    User can send message :)

--- src/Chat.js
+++ src/Chat.js
@@ -10,32 +10,60 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  Dimensions,
+  TextInput,
+  TouchableHighlight,
+  ListView,
 } from 'react-native';
-
-
-
-import Messages from './Components/Messages';
-import MessageInput from './Components/MessageInput';
+var {height, width} = Dimensions.get('window');
 
 export default class Chat extends Component {
   constructor(props){
+
     super(props);
+    ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
-  }
-
-  _scrollToInput (reactNode: any) {
-    // Add a 'scroll' ref to your ScrollView
-    this.refs.scroll.scrollToFocusedInput(reactNode)
+    this.state = {
+      text: null,
+      messages: [],
+      dataSource: ds.cloneWithRows({}),
+    };
   }
 
   render() {
     return (
       <View style={styles.container}>
-          <Messages type="received" content="Salut, ca va ?" />
-          <Messages type="sended" content="Hey, ca fait longtemps ! Ca va bien et toi ?" />
-          <Messages type="received" content="..." />
-          <MessageInput />
+          <ListView
+            dataSource={this.state.dataSource}
+            renderRow={(m) => <View style={mStyle.messages}><View style={mStyle[m.type]}><Text>{m.message}</Text></View></View>}
+            renderHeader={() => <View><Text>Hello World !</Text></View>}
+            enableEmptySections
+          />
+        <View style={inputStyle.container}>
+          <TextInput
+            ref="messageInput"
+                style={inputStyle.textInput}
+                placeholder={'Send a text message'}
+                onChangeText={(text) => this.setState({text})}
+                value={this.state.text}
+            />
+            <TouchableHighlight onPress={() => {
+                if(this.state.text != null) {
+                  this.state.messages.push({
+                    message: this.state.text,
+                    type: 'sended'
+                  })
+                  this.setState({dataSource: ds.cloneWithRows(this.state.messages)})
+                  this.setState({text: null})
+                }
+
+              }
+            }
+              style={inputStyle.button}
+            >
+                <Text>Text</Text>
+            </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -48,17 +76,55 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+});
+
+const inputStyle = StyleSheet.create({
+  container: {
+    width: width,
+    backgroundColor: '#F5FCFF',
+    position: 'absolute',
+    bottom: 0,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
+  textInput: {
+    flex: 1,
+    height: 47,
+    marginRight: 100,
+    paddingLeft: 10,
+    fontSize: 17,
+    backgroundColor: 'green',
+    borderWidth: 0,
+    borderRadius: 4,
+  },
+  button: {
+    width: 50,
+    backgroundColor: 'red',
+  },
+});
+
+const mStyle = StyleSheet.create({
+  messages: {
+    width:width,
+    flex: 1,
+    marginTop: 5,
     marginBottom: 5,
+    flexDirection:'row',
   },
-  textinput: {
-      backgroundColor: 'red'
-  }
+  sended: {
+    width:width,
+    flex: 1,
+    marginLeft: 70,
+    marginRight: 5,
+    backgroundColor:'deepskyblue',
+    padding: 5,
+    borderRadius: 10,
+  },
+  received: {
+    width:width,
+    flex: 1,
+    marginLeft: 5,
+    marginRight: 70,
+    backgroundColor: 'ghostwhite',
+    padding: 5,
+    borderRadius: 10,
+  },
 });
